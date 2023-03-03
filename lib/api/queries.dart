be6152afa725a35e058a6632f8b5d97fa10575c8
Mustafa <mmermmus123@gmail.com>
2023-03-03 23:41:38 +0300

    update

--- lib/api/queries.dart
+++ lib/api/queries.dart
@@ -8,3 +8,11 @@ class Queries {
     "allArchitecture": "$artQueryRoot arts.allArchitecture();",
   };
 }
+
+enum Que { allPaints, allStatues, allDecorative, allArchitecture }
+
+extension ExtensionQueries on Que {
+  static const String artQueryRoot =
+      "select art_id,art_name,art_artist_id,art_material_id,art_movement_id,art_about_id,artist_name,material_name,movement_name,about_text,about_img from";
+  String get getQue => "$artQueryRoot arts.$name();";
+}
