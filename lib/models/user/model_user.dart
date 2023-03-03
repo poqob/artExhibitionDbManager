ab059e7d445d2a0a9a8b891b13192058ca4ebe27
Mustafa <mmermmus123@gmail.com>
2023-03-03 23:21:50 +0300

    models updated

---
+++ lib/models/user/model_user.dart
@@ -0,0 +1,11 @@
+class User {
+  late final int? userId;
+  late final String? userName;
+  late final String? userPassword;
+  late final int? userCountryId;
+  late final int? userCityId;
+  late final int? userAvatarId;
+
+  User(this.userId, this.userName, this.userPassword, this.userCountryId,
+      this.userCityId, this.userAvatarId);
+}
