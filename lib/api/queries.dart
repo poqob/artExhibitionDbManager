547166741334ad95e6f971d2ca598b2878eac85c
Mustafa <mmermmus123@gmail.com>
2023-03-03 23:52:46 +0300

    update Que class

--- lib/api/queries.dart
+++ lib/api/queries.dart
@@ -1,18 +1,23 @@
-class Queries {
-  static const String artQueryRoot =
-      "select art_id,art_name,art_artist_id,art_material_id,art_movement_id,art_about_id,artist_name,material_name,movement_name,about_text,about_img from";
-  static Map<String?, String?> queries = <String?, String?>{
-    "allPaints": "$artQueryRoot arts.allPaints();",
-    "allStatues": "$artQueryRoot arts.allStatues();",
-    "allDecorative": "$artQueryRoot arts.allDecorative();",
-    "allArchitecture": "$artQueryRoot arts.allArchitecture();",
-  };
+enum Que {
+  allPaints,
+  allStatues,
+  allDecorative,
+  allArchitecture,
+  allArtists,
+  allMuseums
 }
-
-enum Que { allPaints, allStatues, allDecorative, allArchitecture }
 
 extension ExtensionQueries on Que {
   static const String artQueryRoot =
       "select art_id,art_name,art_artist_id,art_material_id,art_movement_id,art_about_id,artist_name,material_name,movement_name,about_text,about_img from";
-  String get getQue => "$artQueryRoot arts.$name();";
+  String get getQue {
+    switch (name) {
+      case "allArtists":
+        return "select * from artists.allArtists();";
+      case "allMuseums":
+        return "select * from museums.allMuseums();";
+      default:
+        return "$artQueryRoot arts.$name();";
+    }
+  }
 }
