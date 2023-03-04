708ae9f786342775545606706da71dc6230c5480
Mustafa <mmermmus123@gmail.com>
2023-03-04 15:50:56 +0300

    note to future

--- lib/models/user/model_user.dart
+++ lib/models/user/model_user.dart
@@ -1,3 +1,6 @@
+// This feature will be used in the next versions.
+// the feature: user profile.
+// user datas which is like total consumed time in app will be added. postgresql user-data table!
 class User {
   late final int? userId;
   late final String? userName;
